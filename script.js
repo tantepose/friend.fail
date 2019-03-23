7421c5eda32bd24366fb021960b63134c16c7e29
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2019-03-23 15:29:05 +0100

    old restyle uploaded cause why not

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
  | | | |  | |  __/ | | | (_| |_| || (_| | | |
  |_| |_|  |_|\___|_| |_|\__,_(_)_| \__,_|_|_|
 
-(C)2017 olepetterstokke.no
+(C) 2017 olepetterstokke.no
 
 */
 
@@ -22,75 +22,66 @@ var inputCounter = -1;					//teller for å lage/slette navne-tekstboksene
 var currentQuestions = [];				//lagrer spørsmåla for å hente tilbake til resultater
 var sortedPlayers = [];                 //array for poengsortert vinnerliste
 
-function Player(name,score,answers)	    //spilleren-objekt, ett nytt per spiller, inn i players-array
-{
+//spilleren-objekt, ett nytt per spiller, inn i players-array
+function Player(name,score,answers) {
   this.name = name;
   this.score = score;
   this.answers = [];
 
-  this.calculateMyScore = function()	//regne ut denne spillerens poeng
-  {
-    i=0;
-    while(i<5)
-    {
-        if (players[currentFocus].answers[i] == this.answers[i])
-            {
-                this.score++;
-            }
-        i++;
+  //regne ut denne spillerens poeng
+  this.calculateMyScore = function() {
+    for (i = 0; i < 5; i++) {
+        if (players[currentFocus].answers[i] == this.answers[i]) {
+            this.score++;
+        }
     }
   }
 }
 
-window.onload = function()	//dokumentet lastes
-{
+//dokumentet lastes
+window.onload = function() { 
     $("#playArea, #button-answer, #button-newRound, #button-newGame, #button-exitGame, #button-back, #menu-help, #menu-about").hide();
     hideQuestions();
     $("#topText").html("Add your players (2-6):");
 }
 
-function menuPlay() //menyen
-{
+//menyen
+function menuPlay() {
     $("#menu").hide();
     $("#playArea").show();
 }
 
-function menuAbout()    //menyen
-{
+//menyen
+function menuAbout() { 
     $("#button-startGame, #button-help, #button-about").hide();
     $("#menu-about, #button-back").show();
     $("#span-Questions").html(questions.length);
 }
 
-function menuHelp() //menyen
-{
+//menyen
+function menuHelp() { 
     $("#button-startGame, #button-help, #button-about").hide();
     $("#menu-help, #button-back").show();
 }
 
-function menuBack() //menyen
-{
+//menyen
+function menuBack() { 
     $("#menu-about, #menu-help, #button-back").hide();
     $("#button-startGame, #button-help, #button-about").show();
 }
 
-function confirmPlayers()	//TRYKK på ok-knapp på startskjerm
-{
+//TRYKK på ok-knapp på startskjerm
+function confirmPlayers() { 
 	writeDebug(arguments.callee.name);
 
     //legge til spillerne
-    if (inputCounter>=1)
-    {
-        if (document.getElementById("input0").value && document.getElementById("input1").value)
-        {
-            i=0;
-            while (i<document.getElementById("textFields").childNodes.length)
-            {
-                if (document.getElementById("input"+i).value)
-                {
+    if (inputCounter>=1) {
+        if (document.getElementById("input0").value && document.getElementById("input1").value) {
+
+            for (i = 0; i < document.getElementById("textFields").childNodes.length; i++) {
+                if (document.getElementById("input"+i).value) {
                     players.push(new Player (document.getElementById("input"+i).value,0));
                 }
-                i++;
             }
 
             playerCount = players.length;
@@ -98,20 +89,14 @@ function confirmPlayers()	//TRYKK på ok-knapp på startskjerm
             turn = 1;
 
 			//fjerne tekstfelt
-            e=0;
-            while (e<=inputCounter)
-            {
-                $("#input"+e).remove();
-                e++;
+            for (i = 0; i <= inputCounter; i++) {
+                $("#input" + i).remove();
             }
 
             //vise spillernavn uten å regne ut poeng, som gir bug første gang
             $("#scoreCounter").html("");
-            i=0;
-            while (i<playerCount)
-            {
+            for (i = 0; i < playerCount; i++) {
                 document.getElementById("scoreCounter").innerHTML+=players[i].name.substring(0,3).toUpperCase() + ":" + players[i].score + " ";
-                i++;
             }
 
             $("#button-add").hide();
@@ -120,50 +105,43 @@ function confirmPlayers()	//TRYKK på ok-knapp på startskjerm
             startNewRound();
         }
 
-        else
-        {
+        else {
             $("#topText").html("Add your players (at least 2):");
         }
     }
 
-    else
-    {
+    else {
         $("#topText").html("Add your players (at least 2):");
     }
 }
 
-function confirmAnswers()	//TRYKK på svareknapp, styrer rundene og hele logikken
-{
+//TRYKK på svareknapp, styrer rundene og hele logikken
+function confirmAnswers() { 
 	writeDebug(arguments.callee.name);
 
-    i = 1;      //alle svar inn i objekt
-    while (i<6)
-    {
+    //alle svar inn i objekt
+    for (i = 1; i < 6; i++) {
         players[currentPlayer].answers.push(document.getElementById("yes"+i).checked);
-        i++;
     }
 
-    if (currentPlayer==playerCount-1)   //hoppe fra eventuell sistemann til førstemann
-    {
+    if (currentPlayer==playerCount-1) { //hoppe fra eventuell sistemann til førstemann
         currentPlayer=0;
     }
-    else                                //gå fra forrigemann til nestemann vanligvis
-    {
+
+    else { //gå fra forrigemann til nestemann vanligvis
         currentPlayer++;
     }
 
-    if (turn<playerCount-1)               //alle svar ikke avgitt
-    {
+    if (turn<playerCount-1) { //alle svar ikke avgitt
         startNewTurn();
     }
-    else if (turn==playerCount-1)
-    {
-        endRound();                     //alle svar avgitt, avslutt runda
+    else if (turn==playerCount-1) { //alle svar avgitt, avslutt runda
+        endRound();
     }
 }
 
-function startNewTurn ()	//starte ny turn, ny spiller skal svare
-{
+//starte ny turn, ny spiller skal svare
+function startNewTurn () { 
 	writeDebug(arguments.callee.name);
 
     turn++;
@@ -172,16 +150,15 @@ function startNewTurn ()	//starte ny turn, ny spiller skal svare
     $("#topText").html(players[currentPlayer].name + ", guess " + players[currentFocus].name + "'s answers, in secret:</br>");
 }
 
-function endRound ()	//round er ferdig, resultater skal gis
-{
+//round er ferdig, resultater skal gis
+function endRound () { 
 	writeDebug(arguments.callee.name);
 
-    if (round==totalRounds)     //om alle av x runder spilt, ett game spilt
-    {
+    if (round==totalRounds) { //om alle av x runder spilt, ett game spilt
         endGame();
     }
-    else                        //bare runda er slutt, resultater og newround-knapp skal vises
-    {
+
+    else { //bare runda er slutt, resultater og newround-knapp skal vises
         hideQuestions();
         displayScores();
         writeEndRoundText();
@@ -191,19 +168,18 @@ function endRound ()	//round er ferdig, resultater skal gis
     }
 }
 
-function startNewRound ()	//TRYKK på newround-knapp, eller fra newgame-knapp/funksjon
-{
+//TRYKK på newround-knapp, eller fra newgame-knapp/funksjon
+function startNewRound () {
 	writeDebug(arguments.callee.name);
 
     round++;
     turn = 0;
 
-    if (currentFocus==playerCount-1)
-    {
+    if (currentFocus==playerCount-1) {
         currentFocus=0;
     }
-    else
-    {
+
+    else {
         currentFocus++;
     }
 
@@ -222,8 +198,8 @@ function startNewRound ()	//TRYKK på newround-knapp, eller fra newgame-knapp/fu
     uncheckAll();
 }
 
-function endGame()	//et game er slutt
-{
+//et game er slutt
+function endGame() {
 	writeDebug(arguments.callee.name);
 
     hideQuestions();
@@ -236,8 +212,8 @@ function endGame()	//et game er slutt
     $("#button-newGame, #button-exitGame").show();
 }
 
-function startNewGame ()	//TRYKK på nytt game-knapp, starte nytt game
-{
+//TRYKK på nytt game-knapp, starte nytt game
+function startNewGame () {
 	writeDebug(arguments.callee.name);
 
     game++;
@@ -246,55 +222,51 @@ function startNewGame ()	//TRYKK på nytt game-knapp, starte nytt game
     startNewRound();
 }
 
-function writeEndGameResults ()     //skrive teksten når alle runder og dermed game er over
-{
+//skrive teksten når alle runder og dermed game er over
+function writeEndGameResults () {
     sortedPlayers.length = 0;               //tømme helt - alle rare måter skyldes bug hvor sorted tok over players
     sortedPlayers = players.slice();        //legge inn fra players - må ha to arrays så bare gjelder scoretekst
-    sortedPlayers.sort(function (a, b)      //sortere etter score, ikke spør, ga mening en gang
-    {
+    
+    sortedPlayers.sort(function (a, b) {     //sortere etter score, ikke spør, ga mening en gang
         return a.score - b.score;
     });
 
-    document.getElementById("topText").innerHTML+="The results are in. Out of "+(game*(playerCount-1)*5)+" points:</br>";
-    i=0;
-    while (i<playerCount)
-    {
-        if (i<playerCount)              //ikke vinner
-        {
+    document.getElementById("topText").innerHTML += "The results are in. Out of " + (game*(playerCount-1)*5) + " points:</br>";
+
+    for (i = 0; i < playerCount; i++) {
+        if (i<playerCount) { //ikke vinner
             document.getElementById("topText").innerHTML+="&#8226; "+sortedPlayers[i].name+" got "+sortedPlayers[i].score+", and failed as a friend.</br>";
-            i++;
         }
-        if (i==(playerCount-1))    //vinner
-        {
+        if (i==(playerCount-1)) { //vinner
             document.getElementById("topText").innerHTML+="&#8226; "+sortedPlayers[i].name+" got "+sortedPlayers[i].score+", and wins!</br></br><hr></br>";
             return;
         }
     }
+
 }
 
-function loadQuestion()	//laste inn fem spørsmål til fem tekster
-{
+//laste inn fem spørsmål til fem tekster
+function loadQuestion()	{
 	writeDebug(arguments.callee.name);
 
-    currentQuestions.length = 0;   //tømme arrayen før laste nye
+    currentQuestions.length = 0; //tømme arrayen før laste nye
     console.log("tømt arrayen, nå: " + currentQuestions);
 
-    i=1;                //må starte på 1 og gå til 6 pga html-objektene som starter på 1
-    while (i<6)
-    {
+    //må starte på 1 og gå til 6 pga html-objektene som starter på 1
+    for (i = 1; i < 6; i++) {
         randomNumber = Math.floor((Math.random() * questions.length));
         randomQuestion = questions[randomNumber];
         currentQuestions.push(randomQuestion);
         $("#question"+i).html("</br>"+randomQuestion);
         $("#questionArea"+i).show();
         questions.splice(randomNumber,1);
-        i++;
     }
+
     console.log("nye spørsmål: " + currentQuestions);
 }
 
-function writeEndRoundText()	//skrive teksten på slutten av hver runde
-{
+//skrive teksten på slutten av hver runde
+function writeEndRoundText() {
 	writeDebug(arguments.callee.name);
 
     window.scrollTo(0,0);
@@ -303,139 +275,117 @@ function writeEndRoundText()	//skrive teksten på slutten av hver runde
 
     console.log("laster spørsmål: " + currentQuestions);
 
-    i=0;                                                                                                //i = aktuelle spørsmålet
-    while(i<5)
-    {
+    //i = aktuelle spørsmålet
+    for (i = 0; i < 5; i++) {
         document.getElementById("topText").innerHTML += "<i>&laquo" + currentQuestions[i] + "&raquo</i></br>"; //dette spørsmålet:
 
-        e=0;                                                                                            //hente fra motspillere (e)
-        while(e<playerCount)
-        {
-            if (e==currentFocus)                                                                        //ikke hente fra current
-            {
+        e = 0; //hente fra motspillere (e)
+        while(e < playerCount) {
+            if (e==currentFocus) { //ikke hente fra current
                 e++;
             }
-            else
-            {
-                if (players[e].answers[i] == players[currentFocus].answers[i])                          //om riktig svar
-                {
+            else {
+                if (players[e].answers[i] == players[currentFocus].answers[i]) { //om riktig svar
                     document.getElementById("topText").innerHTML += "&#8226; " + players[e].name + " got it!</br>";
                 }
-                else                                                                                    //om galt svar
-                {
+                else { //om galt svar
                     document.getElementById("topText").innerHTML += "&#8226; " + players[e].name + " failed.</br>";
                 }
                 e++;
             }
         }
-                                                                                                        //hva fasit er
-        if (players[currentFocus].answers[i])                                                           //jeg agreed
-        {
+ 
+        //hva fasit er
+        if (players[currentFocus].answers[i]) { //jeg agreed
             document.getElementById("topText").innerHTML += "&#8226; I agreed.";
         }
-        else                                                                                            //jeg disagreed
-        {
+        else { //jeg disagreed
             document.getElementById("topText").innerHTML += "&#8226; I disagreed.";
         }
-            document.getElementById("topText").innerHTML += "</br></br><hr></br>";                               //separere bolker
-        i++;
+
+        document.getElementById("topText").innerHTML += "</br></br><hr></br>"; //separere bolker
     }
 }
 
-function displayScores ()	//sette nye poeng i topptekst
-{
+//sette nye poeng i topptekst
+function displayScores () {
 	writeDebug(arguments.callee.name);
 
     //regne ut poeng for hver spiller
-    e=0;
-    while(e<playerCount)
-    {
-        if (e!=currentFocus)
-        {
-            players[e].calculateMyScore();
+    for (i = 0; i < playerCount; i++) {
+        if (i != currentFocus) {
+            players[i].calculateMyScore();
         }
-        e++;
     }
 
     //sette poeng i toppteksten
     $("#scoreCounter").html("");
-    i=0;
-    while (i<playerCount)
-    {
+    for (i = 0; i < playerCount; i++) {
         document.getElementById("scoreCounter").innerHTML += players[i].name.substring(0,3).toUpperCase() + ":" + players[i].score + " ";
-        i++;
     }
 }
 
-function hideQuestions ()	//skjule alle spørsmål
-{
+//skjule alle spørsmål
+function hideQuestions () {
 	writeDebug(arguments.callee.name);
 
-    i=1;
-    while (i<6)
-    {
+    for (i = 1; i < 6; i++) {
         $("#questionArea"+i).hide();
-        i++;
     }
 }
 
-function clearAnswers ()	//fjerne svar fra player-objektet, så nye kan mates inn
-{
+//fjerne svar fra player-objektet, så nye kan mates inn
+function clearAnswers () {
 	writeDebug(arguments.callee.name);
 
-    i=0;
-    while (i<playerCount)
-    {
+    for (i = 0; i < playerCount; i++) {
         players[i].answers=[];
-        i++;
     }
 }
 
-function uncheckAll ()	//fjerne haker fra radioknappene
-{
+//fjerne haker fra radioknappene
+function uncheckAll () {
 	writeDebug(arguments.callee.name);
 
-    i=1;
-    while (i<6)
-    {
+    for (i = 1; i < 6; i++) {
         $("#yes"+i).iCheck('uncheck');
         $("#no"+i).iCheck('uncheck');
-        i++;
     }
 }
 
-function addTextField ()	//legge til navnetekstfelt
-{
+//legge til navnetekstfelt
+function addTextField () {
 	writeDebug(arguments.callee.name);
 
-    if (inputCounter<5)
-    {
+    if (inputCounter<5) {
         inputCounter++;
+
         var newField = document.createElement("input");
             newField.id = "input" + inputCounter;
             newField.type = "text";
             newField.style = "display:none";
             newField.placeholder = "name";
             newField.maxLength = "10";
+
         var element = document.getElementById("textFields");
             element.appendChild(newField);
+
         $("#input"+inputCounter).show()
     }
 }
 
-function exitGame () //TRYKK på ikke flere runder
-{
+//TRYKK på ikke flere runder
+function exitGame () {
     location.reload();
 }
 
-function writeDebug (functionName)	//skriver ut hvilken funksjon som kjøres, og hva statsa er
-{
-	if (functionName=="startNewGame" || functionName=="startNewRound")
-	{
+//skriver ut hvilken funksjon som kjøres, og hva statsa er
+function writeDebug (functionName) {
+	if (functionName=="startNewGame" || functionName=="startNewRound") {
 		console.log("------>"+functionName+"(focus:"+currentFocus+"|game:"+game+"|round:"+round+"|turn:"+turn+")");
-	}
-	else
-	{
+    }
+    
+	else {
 		console.log(functionName+"(focus:"+currentFocus+"|game:"+game+"|round:"+round+"|turn:"+turn+")");
 	}
 }
@@ -448,12 +398,4 @@ function writeDebug (functionName)	//skriver ut hvilken funksjon som kjøres, og
  | | | |  | |  __/ | | | (_| |_| || (_| | | |
  |_| |_|  |_|\___|_| |_|\__,_(_)_| \__,_|_|_|
                                              (C)2017
-
-    speed=300;
-    $("#questionArea1").fadeIn(speed,function(){
-        $("#questionArea2").fadeIn(speed,function(){
-        $("#questionArea3").fadeIn(speed,function(){
-        $("#questionArea4").fadeIn(speed,function(){
-        $("#questionArea5").fadeIn(speed)})})})});
-
 */
